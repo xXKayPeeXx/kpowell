cf64d1c69d765d1ca2bba29d0715da61cbac018c
xXKayPeeXx <khrishawn@hotmail.co.uk>
2016-07-06 17:43:07 +0100

    my work

---
+++ resumeBuilder.js
@@ -0,0 +1,9 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+$("#main").append("Khrishawn <em>Powell</em> ");
+var awesomethoughts="I am Khrishawn and I am AWESOME!";
+console.log(awesomethoughts)
+var funthoughts=awesomethoughts.replace("AWESOME!","FUN");
+$("#main").append(funthoughts);
+console.log(funthoughts);
